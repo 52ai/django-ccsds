249eea845d1ba0870007ff7215d74531a3c0f5e6
52ai <ieeflsyu@outlook.com>
2016-11-25 09:12:16 +0800

    merge 20161125

--- project/mysite/mysite/settings.py
+++ project/mysite/mysite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 '''
 TEMPLATE_DIRS = (
-        os.path.join(BASE_DIR+ '/templates/'),
+        os.path.join(BASE_DIR+ 'templates').replace('\\','/'),
     )
 print TEMPLATE_DIRS
 print os.path.join(BASE_DIR+'/db', 'db.sqlite3')
